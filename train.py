0aec91ceffd4c7b8113e7a326662522a73764536
Todd Nief <tnief@h002.ds.uchicago.edu>
2024-03-12 14:36:34 -0500

    unbreak working

--- train.py
+++ train.py
@@ -73,10 +73,10 @@ with torch.no_grad():
     for data in testloader:
         images, labels = data
         # calculate outputs by running images through the network
-        outputs = net(images)
+        outputs = net(images.to(device))
         # the class with the highest energy is what we choose as prediction
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        correct += (predicted.to("cpu") == labels).sum().item()
 
 print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
